94ebfcc9efe483b4d6f04015cb27ecbcd7fbb1ec
Gareth Tribello <gareth.tribello@gmail.com>
2018-09-26 12:00:16 +0100

    New and updated memo manager.  This is really version 2

--- memoManager.py
+++ memoManager.py
@@ -15,67 +15,80 @@ def copyMemo( number, staff, role, password ) :
     proc.communicate()
 
 
-# Make a dictionary from excel containing the students
-wbp = xw.Book( '/Users/gareth/Desktop/DS/DS-list-2017-2018-all_students.xlsm')
+# Open the worksheet that contains all our useful information on the students
+wbp = xw.Book( '/Users/gareth/Desktop/DS/2018/basic-information.xlsm')
 # Get a sheet from the workbook
 sht = xw.sheets['options']
-# Get data on staff numbers from exccel
+# Get data on staff numbers from exccel and make a dictionary
 n, staffdict, staffdata, staffnumbers = 0, {}, sht.range("G3:G72").value, sht.range("F3:F72").value
 for name in staffdata : 
-    staffdict[ name ] = "Sn@" + str( int(staffnumbers[n]) )
+    staffdict[ name ] = str( int(staffnumbers[n]) )
     n += 1
 
-#for k,n in staffdict.items() : print( k, n )
+# for k,n in staffdict.items() : print( k, n )
+
+# List of possible student requirements
+possible_requirements = ["Green Room", "Individual room", "Cubical 6-8", "font size 18 on A4 paper"]
 
 # Make a dictionary of who teaches each module
-n, modteach, modules, teachers = 0, {}, sht.range("K3:K86").value, sht.range("L3:L86").value
+n, modteach, modules, teachers = 0, {}, sht.range("K3:K82").value, sht.range("L3:L82").value
 for module in modules : 
-    modteach[module] = teachers[n].split()
+    mod = module.replace("*","")
+    modteach[mod] = {}
+    if module.find("*")!=-1 : modteach[mod]["project"] = True
+    else : modteach[mod]["project"] = False
+    modteach[mod]["project_students"] = [] 
+    modteach[mod]["teachers"] = teachers[n].split()
+    for requirement in possible_requirements : modteach[mod][requirement] = []
     # Check we have staff numbers for everyone who teaches a module
-    for staff in modteach[module] :
+    for staff in modteach[mod]["teachers"] :
         if staff not in staffdict : RuntimeError("staff number not found for " + staff)
     n+=1 
 
-#for k, n in modteach.items() : print( k, n )
+# for k, n in modteach.items() : print( k, n )
 
+# Get the list of advisors of study
+advisor_names, advisor_dictionary = sht.range("D3:D12").value, {}
+for advisor in advisor_names :
+    # Open advisors excel sheet
+    shd =  xw.sheets[advisor]
+    # Read data on students
+    n, advisor_dictionary[advisor], s_data = 0, {}, shd.range('B8').expand().value
+    for col in s_data[0] :
+        if col == "fake" : continue
+        tdict = {}
+        tdict["modules"] = []
+        for mod in range(1,7) :
+            if (s_data[mod][n]!=None) & (s_data[mod][n]!="End") & (s_data[mod][n]!="end") : tdict["modules"].append( s_data[mod][n] )
+        advisor_dictionary[advisor][ str(int(col)) ] = tdict 
+        n += 1
+
+# Read in the main sheet
 shd = xw.sheets["main"]
 n, student_dictionary, data = 0, {}, shd.range('B8').expand().value
 for col in data[0] :
-    tdict = {}
+    tdict, advisor = {}, ""
     tdict["supervisor"], tdict["modules"] = [], []
-    if (data[12][n]!=None) & (data[12][n]!="End") : tdict["advisor"] = data[12][n] 
-    if (data[13][n]!=None) & (data[13][n]!="End") : tdict["personal"] = data[13][n]
-    if (data[36][n]!=None) & (data[36][n]!="End") : tdict["supervisor"].append( data[36][n] )
-    if (data[37][n]!=None) & (data[37][n]!="End") : tdict["supervisor"].append( data[37][n] )
-    if (data[38][n]!=None) & (data[38][n]!="End") : tdict["supervisor"].append( data[38][n] )
-    for mod in [16,18,20,22,24,26,28,30,32,34] :
-        if (data[mod][n]!=None) & (data[mod][n]!="End") : tdict["modules"].append( data[mod][n] )
+    if (data[4][n]!=None) & (data[4][n]!="End") : tdict["advisor"], advisor = data[4][n], data[4][n] 
+    if (data[5][n]!=None) & (data[5][n]!="End") : tdict["personal"] = data[5][n]
+    if (data[6][n]!=None) & (data[6][n]!="End") : tdict["supervisor"].append( data[6][n] )
+    if (data[7][n]!=None) & (data[7][n]!="End") : tdict["supervisor"].append( data[7][n] )
+    if (data[8][n]!=None) & (data[8][n]!="End") : tdict["supervisor"].append( data[8][n] )
+    # Now get modules from advisor dictionary
+    if advisor != "" :
+       for module in advisor_dictionary[advisor][ str(int(col)) ]["modules"] : tdict["modules"].append( module )
     student_dictionary[ str(int(col)) ] = tdict 
     n += 1
 
-# wbm = xw.Workbook( '/Users/gareth/Desktop/DS/DS-list-2017-2018-maths.xlsx')
-# n, data = 0, xw.Range( 'Sheet1', 'B3', wkb=wbm ).table.value
-# for col in data[0] :
-#     tdict = {}
-#     tdict["modules"], tdict["supervisor"] = [], []
-#     if (data[3][n]!=None) & (data[3][n]!="End") : tdict["advisor"] = data[3][n] 
-#     if (data[4][n]!=None) & (data[4][n]!="End") : tdict["personal"] = data[4][n]
-#     if (data[23][n]!=None) & (data[23][n]!="End") : tdict["supervisor"].append( data[23][n] )
-#     for mod in [9,11,13,15,17,19,21] :
-#         if (data[mod][n]!=None) & (data[mod][n]!="End") : tdict["modules"].append( data[mod][n] ) 
-#     student_dictionary[ str(int(col)) ] = tdict 
-#     n+=1 
+#print( student_dictionary ) 
 
-# for k, n in student_dictionary.items() : print( k, n )
-
-
-# Now work through all the memos that were downloaded
+# # Now work through all the memos that were downloaded
 os.mkdir("Memos")
 os.mkdir("Allstaff")
-for memo in os.listdir("/Users/gareth/Desktop/DS/Downloadedmemos/") :
+for memo in os.listdir("/Users/gareth/Desktop/DS/2018/Downloadedmemos/") :
     if memo.endswith(".docx") :
        # Get the student number from the memo
-       text = textract.process('/Users/gareth/Desktop/DS/Downloadedmemos/' + memo ).decode("utf-8")
+       text = textract.process('/Users/gareth/Desktop/DS/2018/Downloadedmemos/' + memo ).decode("utf-8")
        start = text.find("Student No:")
        end = text.find("Course:")
        studentno = text[start:end].replace("Student No:","").rstrip().strip()
@@ -86,7 +99,7 @@ for memo in os.listdir("/Users/gareth/Desktop/DS/Downloadedmemos/") :
        thisstudent = student_dictionary[ studentno ] 
 
        # Now copy the memo to my stash of memos and rename to student number
-       shutil.copy( '/Users/gareth/Desktop/DS/Downloadedmemos/' + memo.replace(".docx",".pdf"), "Memos/" + studentno + ".pdf" )
+       shutil.copy( '/Users/gareth/Desktop/DS/2018/Downloadedmemos/' + memo.replace(".docx",".pdf"), "Memos/" + studentno + ".pdf" )
        # Copy the students memo for the advisor of study 
        if "advisor" in thisstudent.keys() : copyMemo( studentno, thisstudent["advisor"], "advisor", staffdict[thisstudent["advisor"]] )
        # Copy the students memo for the personal tutor
@@ -95,6 +108,33 @@ for memo in os.listdir("/Users/gareth/Desktop/DS/Downloadedmemos/") :
        for tsuper in thisstudent["supervisor"] : copyMemo( studentno, tsuper, "supervisor", staffdict[tsuper] )
        # Now make copies for modules
        for module in thisstudent["modules"] :
-           for staff in modteach[module] : 
+           # Nothing to do for computer science modules 
+           if "CSC" in module : continue
+           # Check all things we need to note down
+           for requirement in possible_requirements :
+               if( text.find(requirement)!=-1 ) : modteach[module][requirement].append( studentno )
+           # Check if this is a project module and add to list of project students if it is
+           if modteach[module]["project"] : modteach[module]["project_students"].append( studentno )
+           # Coopy the modules we need
+           for staff in modteach[module]["teachers"] : 
                copyMemo( studentno, staff, module, staffdict[staff] )
        print( "Made memo for student " + studentno + " with memo " + memo )
+
+# Get information for teaching office on green room student in each module
+os.mkdir("Office")
+for module, dicto in modteach.items() :
+    # Get project students
+    if dicto["project"] : 
+       print("STUDENTS TAKING PROJECT MODULE " + module )
+       for student in dicto["project_students"] : print( student )
+    # Print info for teaching office
+    of = open('/Users/gareth/Desktop/DS/2018/Office/' + module + ".txt", "w")
+    for requirement in possible_requirements :
+        if len(dicto[requirement])>0 : 
+           of.write("Requirement: " + requirement + "\n" )
+           of.write("--------------------------------- \n")
+           of.write("\n")
+           for student in dicto[requirement] : of.write( student + " ")
+           of.write("\n")
+           of.write("\n")
+    of.close()
