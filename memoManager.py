d0649fac58aed8afc48a2d4f37516f6023da1aa8
Gareth Tribello <gareth.tribello@gmail.com>
2019-09-24 15:36:19 +0100

    New changes to memo manager

--- memoManager.py
+++ memoManager.py
@@ -16,11 +16,11 @@ def copyMemo( number, staff, role, password ) :
 
 
 # Open the worksheet that contains all our useful information on the students
-wbp = xw.Book( '/Users/gareth/Desktop/DS/2018/basic-information.xlsm')
+wbp = xw.Book( '/Users/gareth/Desktop/DS/2019/basic-information.xlsm')
 # Get a sheet from the workbook
 sht = xw.sheets['options']
 # Get data on staff numbers from exccel and make a dictionary
-n, staffdict, staffdata, staffnumbers = 0, {}, sht.range("G3:G72").value, sht.range("F3:F72").value
+n, staffdict, staffdata, staffnumbers = 0, {}, sht.range("G3:G83").value, sht.range("F3:F83").value
 for name in staffdata : 
     staffdict[ name ] = str( int(staffnumbers[n]) )
     n += 1
@@ -28,12 +28,13 @@ for name in staffdata :
 # for k,n in staffdict.items() : print( k, n )
 
 # List of possible student requirements
-possible_requirements = ["Green Room", "Individual room", "Cubical 6-8", "font size 18 on A4 paper"]
+possible_requirements = ["Green Room", "Individual room", "Cubical 6-8", "font size 18 on A4 paper", "Flexible deadlines for Assignments", "Permission to record lectures/tutorials", "Materials in alternative format", "Adjustments for Group work", "Adjustments for Oral Presentations", "Extra time", "Flexibility with deadlines", "Recording of Lectures"]
 
 # Make a dictionary of who teaches each module
-n, modteach, modules, teachers = 0, {}, sht.range("K3:K82").value, sht.range("L3:L82").value
+n, modteach, modules, teachers = 0, {}, sht.range("K3:K85").value, sht.range("L3:L85").value
 for module in modules : 
-    mod = module.replace("*","")
+    #mod = module.replace("*","")
+    mod = module
     modteach[mod] = {}
     if module.find("*")!=-1 : modteach[mod]["project"] = True
     else : modteach[mod]["project"] = False
@@ -48,20 +49,20 @@ for module in modules :
 # for k, n in modteach.items() : print( k, n )
 
 # Get the list of advisors of study
-advisor_names, advisor_dictionary = sht.range("D3:D12").value, {}
-for advisor in advisor_names :
-    # Open advisors excel sheet
-    shd =  xw.sheets[advisor]
-    # Read data on students
-    n, advisor_dictionary[advisor], s_data = 0, {}, shd.range('B8').expand().value
-    for col in s_data[0] :
-        if col == "fake" : continue
-        tdict = {}
-        tdict["modules"] = []
-        for mod in range(1,7) :
-            if (s_data[mod][n]!=None) & (s_data[mod][n]!="End") & (s_data[mod][n]!="end") : tdict["modules"].append( s_data[mod][n] )
-        advisor_dictionary[advisor][ str(int(col)) ] = tdict 
-        n += 1
+# advisor_names, advisor_dictionary = sht.range("D3:D12").value, {}
+# for advisor in advisor_names :
+#     # Open advisors excel sheet
+#     shd =  xw.sheets[advisor]
+#     # Read data on students
+#     n, advisor_dictionary[advisor], s_data = 0, {}, shd.range('B8').expand().value
+#     for col in s_data[0] :
+#         if col == "fake" : continue
+#         tdict = {}
+#         tdict["modules"] = []
+#         for mod in range(1,7) :
+#             if (s_data[mod][n]!=None) & (s_data[mod][n]!="End") & (s_data[mod][n]!="end") : tdict["modules"].append( s_data[mod][n] )
+#         advisor_dictionary[advisor][ str(int(col)) ] = tdict 
+#         n += 1
 
 # Read in the main sheet
 shd = xw.sheets["main"]
@@ -69,14 +70,16 @@ n, student_dictionary, data = 0, {}, shd.range('B8').expand().value
 for col in data[0] :
     tdict, advisor = {}, ""
     tdict["supervisor"], tdict["modules"] = [], []
-    if (data[4][n]!=None) & (data[4][n]!="End") : tdict["advisor"], advisor = data[4][n], data[4][n] 
-    if (data[5][n]!=None) & (data[5][n]!="End") : tdict["personal"] = data[5][n]
-    if (data[6][n]!=None) & (data[6][n]!="End") : tdict["supervisor"].append( data[6][n] )
-    if (data[7][n]!=None) & (data[7][n]!="End") : tdict["supervisor"].append( data[7][n] )
-    if (data[8][n]!=None) & (data[8][n]!="End") : tdict["supervisor"].append( data[8][n] )
+    for j in range(4,14) :
+        if (data[j][n]!=None) & (data[j][n]!="End") : tdict["modules"].append( data[j][n] )
+    if (data[14][n]!=None) & (data[14][n]!="End") : tdict["advisor"] = data[14][n]  
+    if (data[15][n]!=None) & (data[15][n]!="End") : tdict["personal"] = data[15][n]
+    if (data[16][n]!=None) & (data[16][n]!="End") : tdict["supervisor"].append( data[16][n] )
+    if (data[17][n]!=None) & (data[17][n]!="End") : tdict["supervisor"].append( data[17][n] )
+    if (data[18][n]!=None) & (data[18][n]!="End") : tdict["supervisor"].append( data[18][n] )
     # Now get modules from advisor dictionary
-    if advisor != "" :
-       for module in advisor_dictionary[advisor][ str(int(col)) ]["modules"] : tdict["modules"].append( module )
+    # if advisor != "" :
+    #    for module in advisor_dictionary[advisor][ str(int(col)) ]["modules"] : tdict["modules"].append( module )
     student_dictionary[ str(int(col)) ] = tdict 
     n += 1
 
@@ -85,10 +88,10 @@ for col in data[0] :
 # # Now work through all the memos that were downloaded
 os.mkdir("Memos")
 os.mkdir("Allstaff")
-for memo in os.listdir("/Users/gareth/Desktop/DS/2018/Downloadedmemos/") :
+for memo in os.listdir("/Users/gareth/Desktop/DS/2019/Downloadedmemos/") :
     if memo.endswith(".docx") :
        # Get the student number from the memo
-       text = textract.process('/Users/gareth/Desktop/DS/2018/Downloadedmemos/' + memo ).decode("utf-8")
+       text = textract.process('/Users/gareth/Desktop/DS/2019/Downloadedmemos/' + memo ).decode("utf-8")
        start = text.find("Student No:")
        end = text.find("Course:")
        studentno = text[start:end].replace("Student No:","").rstrip().strip()
@@ -99,7 +102,7 @@ for memo in os.listdir("/Users/gareth/Desktop/DS/2018/Downloadedmemos/") :
        thisstudent = student_dictionary[ studentno ] 
 
        # Now copy the memo to my stash of memos and rename to student number
-       shutil.copy( '/Users/gareth/Desktop/DS/2018/Downloadedmemos/' + memo.replace(".docx",".pdf"), "Memos/" + studentno + ".pdf" )
+       shutil.copy( '/Users/gareth/Desktop/DS/2019/Downloadedmemos/' + memo.replace(".docx",".pdf"), "Memos/" + studentno + ".pdf" )
        # Copy the students memo for the advisor of study 
        if "advisor" in thisstudent.keys() : copyMemo( studentno, thisstudent["advisor"], "advisor", staffdict[thisstudent["advisor"]] )
        # Copy the students memo for the personal tutor
@@ -122,13 +125,15 @@ for memo in os.listdir("/Users/gareth/Desktop/DS/2018/Downloadedmemos/") :
 
 # Get information for teaching office on green room student in each module
 os.mkdir("Office")
+os.mkdir("Allstaff/Patrick_J")
+os.mkdir("Allstaff/Lindsay_V")
 for module, dicto in modteach.items() :
     # Get project students
     if dicto["project"] : 
        print("STUDENTS TAKING PROJECT MODULE " + module )
        for student in dicto["project_students"] : print( student )
     # Print info for teaching office
-    of = open('/Users/gareth/Desktop/DS/2018/Office/' + module + ".txt", "w")
+    of = open('/Users/gareth/Desktop/DS/2019/Office/' + module + ".txt", "w")
     for requirement in possible_requirements :
         if len(dicto[requirement])>0 : 
            of.write("Requirement: " + requirement + "\n" )
@@ -138,3 +143,10 @@ for module, dicto in modteach.items() :
            of.write("\n")
            of.write("\n")
     of.close()
+    # Copy file of teaching office information to all lecturers teaching this module
+    if os.stat('/Users/gareth/Desktop/DS/2019/Office/' + module + ".txt").st_size > 0 :
+       for staff in modteach[module]["teachers"] : shutil.copyfile( '/Users/gareth/Desktop/DS/2019/Office/' + module + ".txt", '/Users/gareth/Desktop/DS/2019/Allstaff/' + staff + '/' + module + ".txt" ) 
+    # Copy overview of student memos for level 1 labs to Jackie
+    if module=="PHY1004" : shutil.copyfile( '/Users/gareth/Desktop/DS/2019/Office/' + module + ".txt", '/Users/gareth/Desktop/DS/2019/Allstaff/Patrick_J/' + module + ".txt" )
+    # Copy overview of student memos for level >1 labs to Victor
+    if module=="PHY2001" or module=="PHY2002" or module=="PHY2003" or module=="PHY2004" : shutil.copyfile( '/Users/gareth/Desktop/DS/2019/Office/' + module + ".txt", '/Users/gareth/Desktop/DS/2019/Allstaff/Lindsay_V/' + module + ".txt" )
