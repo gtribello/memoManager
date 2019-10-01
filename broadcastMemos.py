0d0b61c56543f4355b061d174796efc988b78d52
Gareth Tribello <gareth.tribello@gmail.com>
2019-10-01 11:31:50 +0100

    Some fixes to broadcast Memos script

--- broadcastMemos.py
+++ broadcastMemos.py
@@ -15,10 +15,12 @@ session.headers.update({"Accept": "application/json; odata=verbose"})
 with session as s:
     for target_dir in os.listdir("Allstaff/") :
          if target_dir==".DS_Store" : continue
+         # Check if the directory exits
+         r = s.get( api_url + "/web/GetFolderByServerRelativeUrl('Disability/Memos/" + target_dir + "')")
+         if r.status_code!=200 : raise Exception('failed to find directory ' + target_dir + " on sharepoint")         
+
          # Get a list of files in the folder.
          r = s.get( api_url + "/web/GetFolderByServerRelativeUrl('Disability/Memos/" + target_dir + "')/Files" )
-         if r.status_code!=200 : raise Exception('failed to find directory ' + target_dir + " on sharepoint")
-
          print("In " + target_dir + " found files : ") 
          for sp_file in r.json()['d']['results'] : print(sp_file['Name'])
          dd = input('Do you want to delete these files from ' + target_dir + ": ")
@@ -38,5 +40,33 @@ with session as s:
          for filename in os.listdir("Allstaff/" + target_dir) :
              print( "Uploading " + target_dir + "/" + filename + " to sharepoint")
              with open("Allstaff/" + target_dir + "/" + filename, 'rb') as f :
-                r3 = s.post( api_url + "/web/GetFolderByServerRelativeUrl('Disability/Memos/" + target_dir + "')/Files/add(url='" + filename + "')", headers = {"X-RequestDigest": form_digest},  files = {"file": f}) 
-                # if r3.status_code!=200 : raise Exception('failed to upload ' + filename + " on sharepint")
+                r3 = s.post( api_url + "/web/GetFolderByServerRelativeUrl('Disability/Memos/" + target_dir + "')/Files/add(url='" + filename.replace("*","") + "')", headers = {"X-RequestDigest": form_digest},  files = {"file": f}) 
+                if r3.status_code!=200 : raise Exception('failed to upload ' + filename + " on sharepint")
+
+    # Check if teaching office directory exits
+    r = s.get( api_url + "/web/GetFolderByServerRelativeUrl('Disability/Memos/Teaching_office')")
+    if r.status_code!=200 : raise Exception('failed to find Teaching_office directory on sharepoint')
+
+    # Get a list of files in the folder.
+    r = s.get( api_url + "/web/GetFolderByServerRelativeUrl('Disability/Memos/Teaching_office')/Files" )
+    print("In Teaching_office found files : ")
+    for sp_file in r.json()['d']['results'] : print(sp_file['Name'])
+    dd = input('Do you want to delete these files from Teaching_office : ')
+
+    r2 = s.post( api_url + "/contextinfo")
+    form_digest = r2.json()['d']['GetContextWebInformation']['FormDigestValue']
+    
+    if dd=='y' : 
+       for sp_file in r.json()['d']['results'] :
+           print( "Deleting Teaching_office/" + sp_file['Name'] )
+           r3=s.post( api_url + "/web/GetFileByServerRelativeUrl('/schools/smp/education/Disability/Memos/Teaching_office/" + sp_file['Name'] + "')", headers = {"X-RequestDigest": form_digest, "X-HTTP-Method":"DELETE"} )
+           if r3.status_code!=200 : raise Exception('failed to delete ' + sp_file['Name'] + " on sharepint")
+    
+    # Post all the files
+    for filename in os.listdir("Office") : 
+        print( "Uploading Office/" + filename + " to sharepoint")
+        with open("Office/" + filename, 'rb') as f :
+           r3 = s.post( api_url + "/web/GetFolderByServerRelativeUrl('Disability/Memos/Teaching_office')/Files/add(url='" + filename.replace("*","") + "')", headers = {"X-RequestDigest": form_digest},  files = {"file": f})
+           if r3.status_code!=200 : raise Exception('failed to upload ' + filename + " on sharepint")
+
+
